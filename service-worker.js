829440af32357e87e4bd23d5b55497001c6b556c
YonseiMusicLab <composer1002@gmail.com>
2026-05-20 00:02:32 +0900

    Deploy baby life log PWA

---
+++ service-worker.js
@@ -0,0 +1,98 @@
+const CACHE_NAME = "baby-life-cache-v2.1.0";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        return Promise.allSettled(
+          APP_SHELL.map(function (asset) {
+            return cache.add(asset);
+          })
+        );
+      })
+      .then(function () {
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function (cacheNames) {
+        return Promise.all(
+          cacheNames.map(function (cacheName) {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+            return Promise.resolve();
+          })
+        );
+      })
+      .then(function () {
+        return self.clients.claim();
+      })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .then(function (response) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put("./index.html", responseClone).catch(function () {});
+          });
+          return response;
+        })
+        .catch(function () {
+          return caches.match("./index.html");
+        })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then(function (cachedResponse) {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(request)
+        .then(function (response) {
+          if (!response || response.status !== 200) {
+            return response;
+          }
+
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(request, responseClone).catch(function () {});
+          });
+          return response;
+        })
+        .catch(function () {
+          return caches.match("./index.html");
+        });
+    })
+  );
+});
