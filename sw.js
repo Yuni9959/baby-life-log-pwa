3e8f56566c2beb9181beda9f14daaa106cdecf04
YonseiMusicLab <composer1002@gmail.com>
2026-05-20 15:32:56 +0900

    Complete Phase 2.3 backup and restore

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'baby-log-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icon-192.svg',
+  './icon-512.svg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.map(function(key) {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+          return undefined;
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(cachedResponse) {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).catch(function(error) {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+        throw error;
+      });
+    })
+  );
+});
