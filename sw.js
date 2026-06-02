b720498e05bc30e3e851557d5479898112c513d8
YonseiMusicLab <composer1002@gmail.com>
2026-06-02 16:28:26 +0900

    APPROVED: Phase update 20260602_162824

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "babylog-cache-6.0.0-design-foundation-ui-hotfix";
+const CACHE_NAME = "babylog-cache-6.0.0-design-foundation-ui-ripple";
 const CACHE_PREFIX = "babylog-cache-";
 const OLD_CACHE_PREFIX = "baby-life-log-";
 const PRIMARY_CACHE_NAME = CACHE_NAME;
