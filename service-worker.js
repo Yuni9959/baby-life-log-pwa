5448458b35b41985495224f21585e3a9498c4eca
YonseiMusicLab <composer1002@gmail.com>
2026-06-05 04:04:15 +0900

    APPROVED: Phase update 20260605_040412

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = "6.0.5-mobile-save-fix";
-const CACHE_NAME = "baby-life-log-v6.0.5-mobile-save-fix";
+const APP_VERSION = "6.0.6-analysis-thin-client";
+const CACHE_NAME = "baby-life-log-v6.0.6-analysis-thin-client";
 const APP_SHELL = [
   "./manifest.json",
   `./service-worker.js?v=${APP_VERSION}`,
